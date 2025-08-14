20e0c24b54cf08db73bf236e77572815d9b2414f
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2025-08-14 11:44:01 -0300

    add: begin frontend project

---
+++ frontend/src/pages/Index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+    return(
+        <>
+            <div className="w-screen h-screen bg-black">
+                <h1>Big Bear Cave</h1>
+            </div>
+        </>
+    );
+}
