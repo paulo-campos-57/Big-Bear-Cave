f0d6aa7c0d1398617bf05f1c0e0cd3f712d30cdb
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2025-08-18 07:59:08 -0300

    add: insert site logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
         <>
             <div className="w-screen h-16 bg-darkSlate flex justify-between items-center fixed top-0 left-0 right-0 z-50 font-pressStart box-border px-4">
                 <div className="flex justify-evenly items-center w-auto">
-                    Logo Here
+                    <img src="/images/logoBear.png" className="w-20" />
                     <h1 className="text-xl ml-2 text-offWhite">Big Bear's Cave</h1>
                 </div>
                 <div className="flex justify-evenly items-center gap-4">
